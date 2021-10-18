a88cdb8e63e0c615658b6cfcccfd39f783d05681
AdamuWanganya <wanganyaadamu@gmail.com>
2021-10-18 08:37:47 +0300

    Add function for choosing the gender

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function akanName() {
     let yearOfBirth = document.getElementById("birthday").value;
+    let gender = document.getElementById("gender");
 
+}
 
+function clickgender(male, female) {
+    if (gender.checked) {
+        return gender.value;
+    }
 }
